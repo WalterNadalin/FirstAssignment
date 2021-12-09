aaba6417eab25a43120767ae9b3836deb003a9ed
Walter Nadalin <wanda@login.hpc>
2021-12-09 12:12:09 +0100

    Added 7/8 measures for ring

--- Ring/ring.c
+++ Ring/ring.c
@@ -28,33 +28,34 @@ int main (int argc, char *argv[])
         int my_tag = rank * 10, left_tag = my_tag, right_tag = my_tag; // Initial tags to send out
         int msgleft = rank, msgright = -rank; // Creating the initial messagges to send out
         int left_bfr, right_bfr; // Variables that will contain the received messages from the left and from the right respectively
-        int np = 0, count = 1, flag = 1;
+        int np = 0, count = 1;
+	double start_time = 0, end_time = 0, min_time = 0;	
 
-        float start_time = MPI_Wtime();
-	
-	for(unsigned i = 0; i < 1000000; i++) {
+	for(unsigned i = 0; i < 100000; i++) {
 
-		while(flag) {
+        	start_time = MPI_Wtime();
+
+		do {
 
 			if(rank % 2 == 0) { // If the rank is even then first send and then receive for both the left and right stream
 
 				// Left stream
-				MPI_Ssend(&msgleft, count, MPI_INT, left_prcs, left_tag, MPI_COMM_WORLD); // Sending to the left
+				MPI_Send(&msgleft, count, MPI_INT, left_prcs, left_tag, MPI_COMM_WORLD); // Sending to the left
 				MPI_Recv(&right_bfr, count, MPI_INT, right_prcs, MPI_ANY_TAG, MPI_COMM_WORLD, &right_sts); // Receiving from the right
 
 				// Right stream
-				// MPI_Ssend(&msgright, count, MPI_INT, right_prcs, right_tag, MPI_COMM_WORLD); // Sending to the right
-				// MPI_Recv(&left_bfr, count, MPI_INT, left_prcs, MPI_ANY_TAG, MPI_COMM_WORLD, &left_sts); // Receiving from the left
+				MPI_Send(&msgright, count, MPI_INT, right_prcs, right_tag, MPI_COMM_WORLD); // Sending to the right
+				MPI_Recv(&left_bfr, count, MPI_INT, left_prcs, MPI_ANY_TAG, MPI_COMM_WORLD, &left_sts); // Receiving from the left
 
 			} else { // If the rank is odd then first receive and then send for both the left and right stream
 
 			  	// Left stream
 				MPI_Recv(&right_bfr, count, MPI_INT, right_prcs, MPI_ANY_TAG, MPI_COMM_WORLD, &right_sts); // Receiving from the right
-				MPI_Ssend(&msgleft, count, MPI_INT, left_prcs, left_tag, MPI_COMM_WORLD); // Sending to the left
+				MPI_Send(&msgleft, count, MPI_INT, left_prcs, left_tag, MPI_COMM_WORLD); // Sending to the left
 
 				// Right stream
-				// MPI_Recv(&left_bfr, count, MPI_INT, left_prcs, MPI_ANY_TAG, MPI_COMM_WORLD, &left_sts); // Receiving from the left
-			   	// MPI_Ssend(&msgright, count, MPI_INT, right_prcs, right_tag, MPI_COMM_WORLD); // Sending to the right
+				MPI_Recv(&left_bfr, count, MPI_INT, left_prcs, MPI_ANY_TAG, MPI_COMM_WORLD, &left_sts); // Receiving from the left
+			   	MPI_Send(&msgright, count, MPI_INT, right_prcs, right_tag, MPI_COMM_WORLD); // Sending to the right
 
 			}
 
@@ -73,29 +74,28 @@ int main (int argc, char *argv[])
 			left_tag = right_sts.MPI_TAG;
 			right_tag = left_sts.MPI_TAG;
 
-			if(left_tag == my_tag) flag = 0;
-
 			// Increasing the count of messages received
 			MPI_Get_count(&right_sts, MPI_INT, &recvd_cnt);
 			np += recvd_cnt;
 			MPI_Get_count(&left_sts, MPI_INT, &recvd_cnt);
 			np += recvd_cnt;
 
-		}
+		} while(left_tag != my_tag);
 
-		flag = 1;
-	
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		end_time = MPI_Wtime();
+
+		min_time += (end_time - start_time);	
+		
 	}
-
-	// MPI_Barrier(MPI_COMM_WORLD);
-	float end_time = MPI_Wtime();
 
         info_file = fopen("info.txt", "a");
         fprintf(info_file, "I am process %d and i have received %d messages. My final messages have tag %d and value %d,%d\n", rank, np, left_tag, left_bfr, right_bfr);
         fclose(info_file);
 
-        time_file = fopen("time.dat", "a");
-        fprintf(time_file, "%d %d %f\n", size, rank, end_time - start_time);
+        time_file = fopen("time8.dat", "a");
+        fprintf(time_file, "%d %d %f\n", size, rank, min_time);
         fclose(time_file);
 
         MPI_Finalize();
--- Ring/run_ring.sh
+++ Ring/run_ring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -q dssc
-#PBS -l nodes=1:ppn=16
+#PBS -l nodes=2:ppn=24
 #PBS -l walltime=00:10:00
 
 cd $PBS_O_WORKDIR/
@@ -9,7 +9,8 @@ rm time.dat
 rm info.txt
 module load openmpi-4.1.1+gnu-9.3.0
 
-for i in {2..16}
+for i in {2..48}
+
 do
   	mpirun -np $i ./ring.x
 done
